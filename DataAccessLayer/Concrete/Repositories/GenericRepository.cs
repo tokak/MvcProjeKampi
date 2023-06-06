0c7c5999d471d23e64ae1a428923f41ce49c81d4
murat <murat@DESKTOP-02OBGOV>
2023-06-06 23:46:53 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿
+using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context c = new Context();
+        DbSet<T> _object;//T değerine gelen sınıfı tutuyor
+
+        public GenericRepository()
+        {
+            //T sınıfına karşılık olarak gelen sınıfı bulma
+            _object = c.Set<T>();
+        }
+
+
+        public void Delete(T p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(T p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<T> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
